81894b15b42af3aa475a21614020e1e736b97aa9
Julien Hubert <julien.hubert1@univ-reims.fr>
2020-03-05 13:31:40 +0100

    Inject YumlController correctly

--- lib/Onurb/Bundle/YumlBundle/Controller/YumlController.php
+++ lib/Onurb/Bundle/YumlBundle/Controller/YumlController.php
@@ -2,8 +2,9 @@
 
 namespace Onurb\Bundle\YumlBundle\Controller;
 
+use Psr\Container\ContainerInterface;
 use Onurb\Bundle\YumlBundle\Yuml\YumlClient;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * Utility to generate Yuml compatible strings from metadata graphs
@@ -14,8 +15,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  * @author  Bruno Heron <herobrun@gmail.com>
  * @author  Marco Pivetta <ocramius@gmail.com>
  */
-class YumlController extends Controller
+class YumlController extends AbstractController
 {
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
     /**
      * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
@@ -24,13 +32,13 @@ class YumlController extends Controller
         /** @var YumlClient $yumlClient */
         $yumlClient = $this->get('onurb_yuml.client');
 
-        $showDetailParam    = $this->container->getParameter('onurb_yuml.show_fields_description');
-        $colorsParam        = $this->container->getParameter('onurb_yuml.colors');
-        $notesParam         = $this->container->getParameter('onurb_yuml.notes');
-        $styleParam         = $this->container->getParameter('onurb_yuml.style');
-        $extensionParam     = $this->container->getParameter('onurb_yuml.extension');
-        $directionParam     = $this->container->getParameter('onurb_yuml.direction');
-        $scale              = $this->container->getParameter('onurb_yuml.scale');
+        $showDetailParam    = $this->getParameter('onurb_yuml.show_fields_description');
+        $colorsParam        = $this->getParameter('onurb_yuml.colors');
+        $notesParam         = $this->getParameter('onurb_yuml.notes');
+        $styleParam         = $this->getParameter('onurb_yuml.style');
+        $extensionParam     = $this->getParameter('onurb_yuml.extension');
+        $directionParam     = $this->getParameter('onurb_yuml.direction');
+        $scale              = $this->getParameter('onurb_yuml.scale');
 
         return $this->redirect(
             $yumlClient->getGraphUrl(
